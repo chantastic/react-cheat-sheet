e4048e32875383466abcf79f0c9ffe09165e9e9b
Michael Chan <mijoch@gmail.com>
2015-12-14 15:24:11 -0800

    chore: cleanup category buttons

---
+++ js/CategoryButton.js
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react';
+import CategoryLabel from './CategoryLabel';
+
+function constantizeName (name) {
+  return name.replace(/[^a-z0-9]/gi,'').toUpperCase();
+}
+
+export default class CategoryButton extends Component {
+  render () {
+    const {
+      color,
+      active,
+      name,
+      nameStyle,
+      onToggle
+    } = this.props;
+
+    return (
+      <CategoryLabel
+        color={color}
+        active={active}
+      >
+        <span style={nameStyle}>{name + ' '}</span>
+        <input
+          type="checkbox"
+          checked={active}
+          onChange={e => onToggle(constantizeName(name), e.target.checked)}
+        />
+      </CategoryLabel>
+    );
+  }
+}
+
+CategoryButton.propTypes = {
+  color: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  onToggle: PropTypes.func.isRequired,
+  nameStyle: PropTypes.shape({
+    color: PropTypes.string
+  })
+}
