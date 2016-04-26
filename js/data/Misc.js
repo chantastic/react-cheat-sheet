a23e3184f3bd20bef60d2b6806de73ca01fa8c51
Michael Chan <mijoch@gmail.com>
2016-04-26 09:24:21 -0700

    feat: add context entries

--- js/data/Misc.js
+++ js/data/Misc.js
@@ -32,6 +32,23 @@ const Greeting = (props) => (
     notSupported: "ref",
     module: "react",
   },
+  {
+    name: "Stateless function syntax (with context)",
+    example: `// ES5
+function Greeting (props, context) {
+  return <div>{context.greeting} {props.name}.</div>;
+}
+Greeting.contextTypes = { greeting: React.PropTypes.string };
+
+// ES2015
+const Greeting = (props, context) => (
+  <div>Hello {props.name}</div>
+);
+Greeting.contextTypes = { greeting: React.PropTypes.string };`,
+    reference: "https://facebook.github.io/react/docs/context.html#referencing-context-in-stateless-functional-components",
+    notSupported: "ref",
+    module: "react",
+  },
 ];
 
 export default extendWithDefaults(data, {
--- js/data/React_Context.js
+++ js/data/React_Context.js
@@ -2,22 +2,62 @@ import extendWithDefaults from "../assignSharedDefaults";
 
 const data = [
   {
+    name: "Context (example)",
+    example: `var Cowboy = React.createClass({
+  childContextTypes: {
+    greeting: React.PropTypes.string
+  },
+
+  getChildContext: function () {
+    return { greeting: "Howdy"};
+  },
+
+  render: function () {
+    return <div>{this.props.children}</div>
+  }
+});
+
+var Greeting = React.createClass({
+  contextTypes: {
+    greeting: React.PropTypes.string
+  },
+
+  render: function () {
+    return <div>{this.context.greeting} {this.props.name}.</div>
+  }
+});
+
+// <Greeting name="Michael" />
+// => Michael.
+
+// <Cowboy><Greeting name="Michael" /></Cowboy>
+// => Howdy Michael.
+`,
+    reference: "https://facebook.github.io/react/docs/context.html",
+  },
+  {
     name: "contextTypes",
-    example: `contextTypes: {
+    example: `// add to the context-aware component
+
+contextTypes: {
   color: React.PropTypes.string
 },`,
     reference: "https://facebook.github.io/react/docs/context.html",
   },
   {
     name: "childContextTypes",
-    example: `childContextTypes: {
+    example: `// add to the context provider
+
+childContextTypes: {
   color: React.PropTypes.string
 },`,
     reference: "https://facebook.github.io/react/docs/context.html",
   },
   {
     name: "getChildContext",
-    example: `getChildContext: function() {
+    example: `// add to the context provider
+
+getChildContext: function() {
   return {color: "purple"};
 }`,
     reference: "https://facebook.github.io/react/docs/context.html",
