c779685877c433c34b28ef7bd63bf5e1f7db5c75
Michael Chan <mijoch@gmail.com>
2016-04-26 09:22:57 -0700

    refactor: move lifecycle chart to LIFECYCLE

--- js/data/Misc.js
+++ js/data/Misc.js
@@ -1,72 +1,4 @@
-import React from "react";
 import extendWithDefaults from "../assignSharedDefaults";
-
-const lifecycleMethods = [
-  {
-    name: "componentWillMount",
-    setStateTriggersUpdate: "no",
-    calledFor: "initial render()",
-    useFor: ".",
-  },
-  {
-    name: "componentDidMount",
-    setStateTriggersUpdate: "no",
-    calledFor: "initial render()",
-    useFor: "async setup, refs, DOM manipulation",
-  },
-  {
-    name: "componentWillReceiveProps",
-    setStateTriggersUpdate: "no",
-    calledFor: "new props",
-    useFor: "respond to props before update",
-  },
-  {
-    name: "shouldComponentUpdate",
-    setStateTriggersUpdate: "yes",
-    calledFor: "new props/state",
-    useFor: "performance. return false to skip update",
-  },
-  {
-    name: "componentWillUpdate",
-    setStateTriggersUpdate: "n/a",
-    calledFor: "new props/state",
-    useFor: "preparation before update",
-  },
-  {
-    name: "componentDidUpdate",
-    setStateTriggersUpdate: "yes",
-    calledFor: "new props/state",
-    useFor: "DOM manipulation after update",
-  },
-  {
-    name: "componentWillUnmount",
-    setStateTriggersUpdate: "n/a",
-    calledFor: "unmounting",
-    useFor: "cleanup of componentDidMount setup",
-  },
-];
-
-const LifecycleMethodsChart = () => (
-  <table>
-    <thead>
-      <tr>
-        <th>name</th>
-        <th>called for</th>
-        <th>calling setState() triggers an update</th>
-      </tr>
-    </thead>
-
-    <tbody>
-      {lifecycleMethods.map((event, i) => (
-        <tr key={i}>
-          <td data-label="name">{event.name}</td>
-          <td data-label="called for">{event.calledFor}</td>
-          <td data-label="calling setState() triggers an update">{event.setStateTriggersUpdate}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
 
 const data = [
   {
@@ -86,14 +18,9 @@ const data = [
     notSupported: "Stateless function syntax",
   },
   {
-    name: "Lifecycle methods",
-    chart: <LifecycleMethodsChart />,
-    example: "",
-  },
-  {
     name: "Stateless function syntax",
     example: `// ES5
-function Greeting(props) {
+function Greeting (props) {
   return <div>Hello {props.name}</div>;
 }
 
