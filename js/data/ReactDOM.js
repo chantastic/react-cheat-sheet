12aecbb269cb1fa94fe93d1de525815c6aeff781
Michael Chan <mijoch@gmail.com>
2016-07-18 18:42:20 -0700

    WIP

--- js/data/ReactDOM.js
+++ js/data/ReactDOM.js
@@ -5,6 +5,7 @@ const data = [
     name: "render",
     example: `ReactDOM.render(<MyComponent />, document.getElementById('MyComponent'));`,
     reference: "http://facebook.github.io/react/docs/top-level-api.html#reactdom.render",
+    playground: "http://jsbin.com/rivelop/edit?html,js,output",
   },
   {
     name: "findDOMNode",
