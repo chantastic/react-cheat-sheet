1dece1aa2c63395b89bcf6b4a839530aa9a58f39
Michael Chan <mijoch@gmail.com>
2017-04-11 10:48:33 -0700

    examples: update SPEC examples

--- src/data/React_Spec.js
+++ src/data/React_Spec.js
@@ -3,55 +3,15 @@ import extendWithDefaults from "../assignSharedDefaults";
 const data = [
   {
     name: "render",
-    example: `render: function () {
+    example: `render() {
   return <div />;
 }`,
-    reference: "http://facebook.github.io/react/docs/component-specs.html#render",
-  },
-  {
-    name: "getInitialState",
-    example: `getInitialState: function () {
-  return { key: value };
-}`,
-    reference: "http://facebook.github.io/react/docs/component-specs.html#getinitialstate",
-    notSupported: "React.Component",
-  },
-  {
-    name: "getDefaultProps",
-    example: `getDefaultProps: function () {
-  return { key: value };
-}`,
-    reference: "http://facebook.github.io/react/docs/component-specs.html#getdefaultprops",
-    notSupported: "React.Component",
-  },
-  {
-    name: "propTypes",
-    example: `propTypes: {
-  myProp: PropTypes.bool
-}`,
-    reference: "http://facebook.github.io/react/docs/component-specs.html#proptypes",
-    notSupported: "React.Component",
-  },
-  {
-    name: "mixins",
-    example: `mixins: [ AMixinObject, AnotherMixinObject ];`,
-    reference: "http://facebook.github.io/react/docs/component-specs.html#mixins",
-    notSupported: "React.Component",
-  },
-  {
-    name: "statics",
-    example: `statics: {
-  customMethod: function(foo) {
-    return foo === 'bar';
-  }
-}`,
-    reference: "http://facebook.github.io/react/docs/component-specs.html#statics",
-    notSupported: "React.Component",
+    reference: "https://facebook.github.io/react/docs/react-component.html#render",
   },
   {
     name: "displayName",
     example: `displayName: "MyComponent"`,
-    reference: "http://facebook.github.io/react/docs/component-specs.html#displayname",
+    reference: "https://facebook.github.io/react/docs/react-component.html#displayname",
   },
 ];
 
