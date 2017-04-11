abf995e867c401aa04bbbaf4e70396af5c0a33f2
Michael Chan <mijoch@gmail.com>
2017-04-11 11:06:46 -0700

    examples: update TOPLEVEL examples

--- src/data/React_TopLevel.js
+++ src/data/React_TopLevel.js
@@ -3,77 +3,79 @@ import extendWithDefaults from "../assignSharedDefaults";
 const data = [
   {
     name: "Component",
-    example: `// ES2015
-
-class MyComponent extends React.Component {
-  render () {
+    example: `class MyComponent extends React.Component {
+  render() {
     return <div />;
   }
 }
 `,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.component",
+    reference: "https://facebook.github.io/react/docs/react-api.html#react.component",
   },
   {
-    name: "createClass",
-    example: `var MyComponent = React.createClass({
-  render: function () {
+    name: "PureComponent",
+    example: `class MyComponent extends React.PureComponent {
+  render() {
+    // only updates props and state have not changed.
+    // uses a shallow compare.
+
     return <div />;
   }
-});`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.createclass",
+}
+`,
+    reference: "https://facebook.github.io/react/docs/react-api.html#react.purecomponent",
   },
   {
     name: "createElement",
-    example: `React.createElement('div', props, ...children); // String
+    example: `React.createElement('div', props, children); // tag name
 
-React.createElement(MyComponent, props, ...children); // ReactClass type`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.createelement",
+React.createElement(MyComponent, props, children); // React Component type`,
+    reference: "https://facebook.github.io/react/docs/react-api.html#createelement",
   },
   {
     name: "cloneElement",
     example: `React.cloneElement(element, props, ...children);`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.cloneelement",
+    reference: "https://facebook.github.io/react/docs/react-api.html#cloneelement",
   },
   {
     name: "createFactory",
-    example: `React.createFactory('div'); // String;
+    example: `React.createFactory('div'); // tag name;
 
-React.createFactory(MyComponentClass); // ReactClass type`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.createfactory",
+React.createFactory(MyComponentClass); // React Component type`,
+    reference: "https://facebook.github.io/react/docs/react-api.html#createfactory",
   },
   {
     name: "isValidElement",
     example: `React.isValidElement(MyComponent);`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.isvalidelement",
+    reference: "https://facebook.github.io/react/docs/react-api.html#isvalidelement",
   },
   {
     name: "Children.map",
     example: `React.Children.map(this.props.children, (child, i) => {
     return child;
 })`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.children.map",
+    reference: "https://facebook.github.io/react/docs/react-api.html#react.children.map",
   },
   {
     name: "Children.forEach",
     example: `React.Children.forEach(this.props.children, (child, i) => {
   console.log(child + ' at index: ' + i);
 })`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.children.foreach",
+    reference: "https://facebook.github.io/react/docs/react-api.html#react.children.foreach",
   },
   {
     name: "Children.count",
     example: `React.Children.count(this.props.children);`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.children.count",
+    reference: "https://facebook.github.io/react/docs/react-api.html#react.children.count",
   },
   {
     name: "Children.only",
-    example: `none`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.children.only",
+    example: `React.Children.only(this.props.children);`,
+    reference: "https://facebook.github.io/react/docs/react-api.html#react.children.only",
   },
   {
     name: "Children.toArray",
-    example: `none`,
-    reference: "http://facebook.github.io/react/docs/top-level-api.html#react.children.toarray",
+    example: `React.Children.toArray(this.props.children)`,
+    reference: "https://facebook.github.io/react/docs/react-api.html#react.children.toarray",
   },
 ];
 
