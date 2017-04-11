8cd8f9afe27b94cf41dda18c6082d786b2423bec
Michael Chan <mijoch@gmail.com>
2017-04-11 10:41:13 -0700

    examples: update PROPTYPES

--- src/data/React_PropTypes.js
+++ src/data/React_PropTypes.js
@@ -3,135 +3,158 @@ import extendWithDefaults from "../assignSharedDefaults";
 const data = [
   {
     name: "PropTypes.array",
-    example: `{
-  optionalArray: PropTypes.array,
-  requiredArray: PropTypes.array.isRequired
+    example: `import { array } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalArray: array,
+  requiredArray: array.isRequired
 }`,
   },
   {
     name: "PropTypes.bool",
-    example: `{
-  optionalBoolean: PropTypes.bool,
-  requiredBoolean: PropTypes.bool.isRequired
+    example: `import { bool } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalBoolean: bool,
+  requiredBoolean: bool.isRequired
 }`,
   },
   {
     name: "PropTypes.func",
-    example: `{
-  optionalFunction: PropTypes.func,
-  requiredFunction: PropTypes.func.isRequired
+    example: `import { func } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalFunction: func,
+  requiredFunction: func.isRequired
 }`,
   },
   {
     name: "PropTypes.number",
-    example: `{
-  optionalNumber: PropTypes.number,
-  requiredNumber: PropTypes.number.isRequired
+    example: `import { number } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalNumber: number,
+  requiredNumber: number.isRequired
 }`,
   },
   {
     name: "PropTypes.object",
-    example: `{
-  optionalObject: PropTypes.object,
-  requiredObject: PropTypes.object.isRequired
+    example: `import { object } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalObject: object,
+  requiredObject: object.isRequired
 }`,
   },
   {
     name: "PropTypes.string",
-    example: `{
-  optionalString: PropTypes.string,
-  requiredString: PropTypes.string.isRequired
+    example: `import { string } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalString: string,
+  requiredString: string.isRequired
 }`,
   },
   {
     name: "PropTypes.node",
-    example: `{
-  optionalNode: PropTypes.node,
-  requiredNode: PropTypes.node.isRequired
+    example: `import { node } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalNode: node,
+  requiredNode: node.isRequired
 };
 
 // anything that can be rendered`,
   },
   {
     name: "PropTypes.element",
-    example: `{
-  optionalElement: PropTypes.element,
-  requiredElement: PropTypes.element.isRequired
+    example: `import { element } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalElement: element,
+  requiredElement: element.isRequired
 }`,
   },
   {
     name: "PropTypes.instanceOf",
-    example: `{
-  optionalClass: PropTypes.instanceOf(MyClass),
-  requiredClass: PropTypes.instanceOf(MyClass).isRequired
+    example: `import { instanceOf } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalClass: instanceOf(SomeClass),
+  requiredClass: instanceOf(SomeClass).isRequired
 }`,
   },
   {
     name: "PropTypes.oneOf",
-    example: `{
-  optionalEnum: PropTypes.oneOf(['Thing 1', 'Thing 2']),
-  optionalEnum: PropTypes.oneOf(['Thing 1', 'Thing 2']).isRequired
+    example: `import { oneOf } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalEnum: oneOf(['Thing 1', 'Thing 2']),
+  requiredEnum: oneOf(['Thing 1', 'Thing 2']).isRequired
 }`,
   },
   {
     name: "PropTypes.oneOfType",
-    example: `{
-  optionalUnion: PropTypes.oneOfType([
-    PropTypes.bool,
-    PropTypes.string
-  ]),
+    example: `import { oneOfType, bool, string } from "prop-types";
 
-  requiredUnion: PropTypes.oneOfType([
-    PropTypes.bool,
-    PropTypes.string
-  ]).isRequired,
+MyComponent.propTypes = {
+  optionalUnion: oneOfType([ bool, string ]),
+
+  requiredUnion: oneOfType([ bool, string ]).isRequired,
 }`,
   },
   {
     name: "PropTypes.arrayOf",
-    example: `{
-  optionalArrayOf: PropTypes.arrayOf(PropTypes.string),
-  requiredArrayOf: PropTypes.arrayOf(PropTypes.string).isRequired
+    example: `import { arrayOf, string } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalArrayOfStrings: arrayOf(string),
+  requiredArrayOfStrings: arrayOf(string).isRequired
 }`,
   },
   {
     name: "PropTypes.objectOf",
-    example: `{
-  optionalObjectOf: PropTypes.objectOf(PropTypes.string),
-  requiredObjectOf: PropTypes.objectOf(PropTypes.string).isRequired
-}
+    example: `import { objectOf, string } from "prop-types";
 
-// Type of property values`,
+MyComponent.propTypes = {
+  optionalObjectOfStrings: objectOf(string),
+  requiredObjectOfStrings: objectOf(string).isRequired
+}`,
   },
   {
     name: "PropTypes.shape",
-    example: `{
-  optionalObjectWithShape: PropTypes.shape({
-    age: PropTypes.number,
-    name: PropTypes.string
+    example: `
+import { number, shape, string } from "prop-types";
+
+MyComponent.propTypes = {
+  optionalObjectWithShape: shape({
+    age: number,
+    name: string
   }),
 
-  requiredObjectWithShape: PropTypes.shape({
-    age: PropTypes.number,
-    name: PropTypes.string
+  requiredObjectWithShape: shape({
+    age: number,
+    name: string
   }).isRequired,
 
-  requiredObjectWithRequiredShape: PropTypes.shape({
-    age: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired
+  requiredObjectWithRequiredShape: shape({
+    age: number.isRequired,
+    name: string.isRequired
   }).isRequired,
 }`,
   },
   {
     name: "PropTypes.any",
-    example: `{
-  requiredAny: PropTypes.any.isRequired
+    example: `import { any } from "prop-types";
+
+MyComponent.propTypes = {
+  requiredAny: any.isRequired
 }`,
   },
 ];
 
 export default extendWithDefaults(data, {
-  module: "react",
+  module: "prop-types",
   category: "PROPTYPES",
-  reference: "https://facebook.github.io/react/docs/reusable-components.html#prop-validation",
+  reference: "https://facebook.github.io/react/docs/typechecking-with-proptypes.html#react.proptypes",
 });
