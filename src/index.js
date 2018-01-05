c6dae49c5a8b1a61a09ed60543fccbc84bcdae32
Michael Chan <mijoch@gmail.com>
2018-01-04 21:42:18 -0800

    change require to import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-require("normalize-css");
+import "normalize-css";
 import "./base.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
